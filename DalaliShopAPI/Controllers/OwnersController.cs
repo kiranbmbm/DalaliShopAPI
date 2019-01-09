7f93321400cd19be20d08f2c22b30ac748d1ba55
k.basavaraj.mudhol <k.basavaraj.mudhol@BDC4-L-FY0BVN2>
2019-01-09 16:40:52 +0530

    Test

--- DalaliShopAPI/Controllers/OwnersController.cs
+++ DalaliShopAPI/Controllers/OwnersController.cs
@@ -38,7 +38,7 @@ namespace DalaliShopAPI.Controllers
             DataSet results = new DataSet();
             try
             {
-                //int a = 1, b = 0, c;
+                int a = 1, b = 0, c;
                 //c = a / b; 
                 results = OwnersLogicObj.GetAllOwners(); 
                 return Ok(results);
